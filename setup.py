2fa7111b3fb1559f75d1f6213bdc907244c61e4d
Simon Mutch <smutch@unimelb.edu.au>
2014-01-08 16:01:49 +1100

    Use setup.py to build python3 compatible version

--- setup.py
+++ setup.py
@@ -42,8 +42,9 @@ setup(
         # "Programming Language :: Python :: 2",
         # 'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
     ],
     test_suite='tests',
+    use_2to3=True,
 )
