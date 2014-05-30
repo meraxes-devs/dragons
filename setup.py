fdc39e8f9ee2f076f090500bda244674386971c4
Simon Mutch <smutch@unimelb.edu.au>
2014-05-30 14:12:43 +1000

    Update requirements.txt and setup.py to now hopefully work for clean install

--- setup.py
+++ setup.py
@@ -5,9 +5,17 @@ import os
 import sys
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup
+    from pkgutil import walk_packages
+
+    def find_packages(path=__path__, prefix=""):
+        yield prefix
+        prefix = prefix + "."
+        for _, name, ispkg in walk_packages(path, prefix):
+            if ispkg:
+                yield name
 
 # if sys.argv[-1] == 'publish':
 #     os.system('python setup.py sdist upload')
@@ -25,9 +33,7 @@ setup(
     author='Simon Mutch',
     author_email='smutch.astro@gmail.com',
     url='https://bitbucket.org/smutch/ssimpl',
-    packages=[
-        'ssimpl',
-    ],
+    packages=find_packages(),
     package_dir={'ssimpl': 'ssimpl'},
     include_package_data=True,
     install_requires=requirements,
